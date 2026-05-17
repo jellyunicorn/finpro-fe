bff5282faacbe5c2b9907dfc459023472b6201ca
Teru <218044131+daveteru@users.noreply.github.com>
2026-05-17 16:21:36 +0700

    style: scaffolded dashboard layout

--- src/pages/user-dashboard/UserDashboard.tsx
+++ src/pages/user-dashboard/UserDashboard.tsx
@@ -2,11 +2,11 @@ import SideBar from "../../components/landing-page/SideBar"
 
 export default function UserDashboard() {
   return (
-    <div className="w-full h-dvh bg-[#FBFDFF] flex">
+    <div className="w-full font-dmsans h-dvh bg-[#FBFDFF] flex">
       <SideBar/>
-      <div className="border  h-full flex flex-col flex-1">
-        <div className="border w-full h-15 flex items-center px-10"> breadcrumbs</div>
-        <main className="border flex-1 flex justify-center items-center"> content </main>
+      <div className="  h-full flex flex-col flex-1">
+        <div className="border-b border-[#BAD6F5] text-neutral-400 font-medium w-full h-16 flex items-center px-10"> Home / </div>
+        <main className=" flex-1 flex justify-center items-center"> content </main>
       </div>
     </div>
   );
