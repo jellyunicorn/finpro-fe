0a1a2d82d5d66536fd05a27651e789a46d7f271f
Teru <218044131+daveteru@users.noreply.github.com>
2026-06-17 22:41:38 +0700

    feat:navbar animation improvement

--- src/components/landing-page/Navbar.tsx
+++ src/components/landing-page/Navbar.tsx
@@ -4,16 +4,50 @@ import mainLogoDark from "../../img/svg/main_logo_blue.svg";
 import mainLogoWhite from "../../img/svg/main_logo_white.svg";
 import { useLoginStore } from "../../store/useAppStore";
 import { goToDashboard } from "../../utils/goToDashboard";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { cloudimages } from "../../lib/cloudinary";
 
 export default function Navbar() {
   //animations
   useNavbarScrollAnimation();
+  const boxRef = useRef(null);
+
+  const { contextSafe } = useGSAP({ scope: boxRef });
+
+  const onEnter = contextSafe(() => {
+    gsap.to(boxRef.current, {
+      width: "50%",
+      height: "275px",
+      paddingTop: "28px",
+      borderRadius: "48px",
+      duration: 0.4,
+      ease: "back.out(2, 0.2)",
+    });
+  });
+
+  const onLeave = contextSafe(() => {
+    gsap.to(boxRef.current, {
+      width: "35%",
+      height: "69px",
+      paddingTop: "20px",
+      duration: 0.4,
+      ease: "back.out(1, 0.1)",
+    });
+  });
+
   const user = useLoginStore((state) => state.user);
 
   return (
     <div className="w-full h-fit fixed z-10 flex justify-center">
-      <div className="text-white border border-neutral-50/10 font-dmsans  z-10 inset-x-0 mt-8 mx-10 py-5 rounded-full px-8 w-[100%] lg:w-[35%] transition-all ease-in-out bg-white/10 backdrop-blur-sm ">
-        <nav className=" flex justify-between max-h-10 items-center h-6">
+      <div
+        ref={boxRef}
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeave}
+        className="text-white border flex flex-col animate-bg-color h-17 border-neutral-50/50 font-dmsans  z-10 inset-x-0 mt-8 mx-10 py-5 rounded-[50px] px-8 w-[100%] lg:w-[35%]  bg-white/10 backdrop-blur-sm overflow-hidden"
+      >
+        <nav className=" flex justify-between max-h-10 items-center h-6 mb-7">
           <div className="relative flex ">
             <img
               src={mainLogoWhite}
@@ -32,15 +66,15 @@ export default function Navbar() {
           {!user && (
             <div className="flex items-center gap-5 ">
               <Link to="/login" state={{ from: location.pathname }}>
-                <span className="hover:underline hover:cursor-pointer animate-text-color">
+                <button className="hover:underline hover:cursor-pointer animate-text-color">
                   LOGIN
-                </span>
+                </button>
               </Link>
-<Link to="/register">
+              <Link to="/register">
                 <button className="hover:cursor-pointer hover:bg-blue-900 ease-in transition-all px-5 py-1 h-full rounded-full bg-claundry-blue">
                   REGISTER
                 </button>
-</Link>
+              </Link>
             </div>
           )}
 
@@ -60,6 +94,27 @@ export default function Navbar() {
               </div>
             </button>
           )}
+        </nav>
+        <hr className="opacity-15 animate-text-color" />
+        <nav className="flex justify-between animate-text-color gap-2 mt-3">
+          <div className="flex flex-col gap-2">
+            <button className="font-dmsans w-fit hover:underline text-2xl">
+              About
+            </button>
+            <button className="font-dmsans w-fit hover:underline text-2xl">
+              Service
+            </button>
+            <button className="font-dmsans w-fit hover:underline text-2xl">
+              Pricing
+            </button>
+            <button className="font-dmsans w-fit hover:underline text-2xl">
+              Career
+            </button>
+          </div>
+          <div className=" w-120 h-full flex gap-2 justify-end items-end">
+
+            <img src={cloudimages.navbarillus} className="w-60" alt="" />
+          </div>
         </nav>
       </div>
     </div>
--- src/hooks/useNavBarColourChange.ts
+++ src/hooks/useNavBarColourChange.ts
@@ -10,14 +10,43 @@ export function useNavbarScrollAnimation() {
       trigger: "#hero",
       start: "bottom 80px",
       onEnter: () => {
-        gsap.timeline()
+        gsap
+          .timeline()
           .to("#logo-white", { opacity: 0, duration: 0.4, ease: "power2.out" })
-          .to(".animate-text-color", { color: "#296FDA", duration: 0.4, ease: "power2.out" }, "<");
+          .to(
+            ".animate-bg-color",
+            {
+              backgroundColor: "rgba(255, 255, 255,1)",
+              boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.1)",
+              duration: 0.4,
+              ease: "power2.out",
+            },
+            "<",
+          )
+          .to(
+            ".animate-text-color",
+            { color: "#296FDA", duration: 0.4, ease: "power2.out" },
+            "<",
+          );
       },
       onEnterBack: () => {
-        gsap.timeline()
+        gsap
+          .timeline()
           .to("#logo-white", { opacity: 1, duration: 0.4, ease: "power2.out" })
-          .to(".animate-text-color", { color: "#FFFFFF", duration: 0.4, ease: "power2.out" }, "<");
+          .to(
+            ".animate-bg-color",
+            {
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              duration: 0.4,
+              ease: "power2.out",
+            },
+            "<",
+          )
+          .to(
+            ".animate-text-color",
+            { color: "#FFFFFF", duration: 0.4, ease: "power2.out" },
+            "<",
+          );
       },
     });
   });
