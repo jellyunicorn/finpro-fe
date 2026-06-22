77f2cb02a0764b50c264162b3c6a2c2cf152dccd
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-06-22 11:09:53 +0700

    fix: disable cancel if items picked up

--- src/components/driver-dashboard/jobs-page/ActiveJobCard.tsx
+++ src/components/driver-dashboard/jobs-page/ActiveJobCard.tsx
@@ -65,13 +65,14 @@ export function ActiveJobCard({
               Mark as Complete
             </button>
           )}
-
-          <button
-            className="border-2 border-red-500 text-red-500 px-4 py-2 rounded hover:bg-red-500 hover:text-white transition"
-            onClick={() => onCancel(activeJob.id, activeJob.type)}
-          >
-            Cancel
-          </button>
+          {activeJob.status === "WAITING_FOR_DRIVER" && (
+            <button
+              className="border-2 border-red-500 text-red-500 px-4 py-2 rounded hover:bg-red-500 hover:text-white transition"
+              onClick={() => onCancel(activeJob.id, activeJob.type)}
+            >
+              Cancel
+            </button>
+          )}
         </div>
       </div>
     </div>
