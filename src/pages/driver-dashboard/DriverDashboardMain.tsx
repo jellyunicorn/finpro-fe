7ec04a081339e5f8a0cb3897e3272a028a63f396
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-06-22 11:53:07 +0700

    fix: responsiveness on washing machine img

--- src/pages/driver-dashboard/DriverDashboardMain.tsx
+++ src/pages/driver-dashboard/DriverDashboardMain.tsx
@@ -69,7 +69,7 @@ export default function WorkerDashboardMain() {
         <img
           src={cloudimages.dashboard_img}
           alt="Dashboard illustration"
-          className="hidden lg:block absolute right-0 top-0 h-full max-h-64 object-contain mr-6"
+          className="hidden lg:block absolute right-0 top-0 h-full max-h-64 w-auto object-contain lg:max-xl:max-h-48"
         />
       </div>
 
